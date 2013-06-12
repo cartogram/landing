8eb27decf9352948039dd47db3c9d49c8499d668
Peter Tran <pete.tran02@gmail.com>
2013-06-12 17:15:55 -0400

    Renamed and updated databaseURLUpdater.php for new WP db configuration. Also commented code.
    Signed-off-by: Peter Tran <pete.tran02@gmail.com>

---
+++ dbupdate.php
@@ -0,0 +1,44 @@
+<?php
+    
+    //Database connection information as per wp-config.
+    $DB_NAME = "databasename";
+    $DB_PASSWORD = "root";
+    $DB_USER = "root";
+	$DB_HOST = "localhost";
+    $link = @mysql_connect($DB_HOST
+, $DB_USER, $DB_PASSWORD);
+
+    
+    if (is_resource($link)) {
+
+        //Connect to the database.
+        mysql_select_db($DB_NAME);
+
+        //Retrieve path to this file.
+        $directory = explode("/",dirname(__FILE__));
+        $url = "http://localhost/" . end($directory);
+
+        //Updating the "siteurl" row in wp_options.
+        $query = "UPDATE wp_options 
+        SET option_value='$url'
+        WHERE option_name='siteurl'";
+        $result = mysql_query($query, $link);
+        if ($result) {
+            echo "option_name:siteurl updated with " . $url . "<br>";    } else {
+            echo "Error updating site url to " . $url . ": " . mysql_error();
+        }
+
+        //Updating the "home" row in wp_options.
+        $query2 = "UPDATE wp_options 
+        SET option_value='$url'
+        WHERE option_name='home'";
+        $result2 = mysql_query($query2, $link);
+        if ($result2) {
+            echo "option_name:home updated with " . $url;
+        } else {
+            echo "Error updating home url to " . $url . ": " . mysql_error();
+        }
+    } else {
+        echo "Error establishing connection to the database.";
+    }
+?>
